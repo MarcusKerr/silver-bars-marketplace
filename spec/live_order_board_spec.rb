0d7480b19feb8f19eec8c29955bcf3192aed12d6
Marcus <marcus.kerr94@gmail.com>
2019-01-19 21:20:59 +0000

    Unit: live order board, sell, validate/delegate to order - RED

--- spec/live_order_board_spec.rb
+++ spec/live_order_board_spec.rb
@@ -32,4 +32,23 @@ describe LiveOrderBoard do
       expect { live_order_board.buy(user_id, order_quantity, 'price_per_kg') }.to raise_error('Price per kg must be a number(£)')
     end
   end
+
+  describe '#sell' do
+    it 'delegates to the order object' do
+      expect(mock_order_class).to receive(:register).with(user_id, order_quantity, price_per_kg, sell )
+      live_order_board.sell(user_id, order_quantity, price_per_kg)
+    end
+
+    it 'raises error if invalid user id is submitted' do
+      expect { live_order_board.sell('user_id', order_quantity, price_per_kg) }.to raise_error('User id must be a whole number')
+    end
+
+    it 'raises error if invalid order quantity is submitted' do
+      expect { live_order_board.sell(user_id, 'order_quantity', price_per_kg) }.to raise_error('Order quantity must be a number(kg)')
+    end
+
+    it 'raises error if invalid price is submitted' do
+      expect { live_order_board.sell(user_id, order_quantity, 'price_per_kg') }.to raise_error('Price per kg must be a number(£)')
+    end
+  end
 end
