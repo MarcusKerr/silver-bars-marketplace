0a8d9d8ce7d4b9cf4c6f517242a2d54253f2a7bc
Marcus <marcus.kerr94@gmail.com>
2019-01-19 21:17:20 +0000

    Unit: live order board, buy - RED

--- spec/live_order_board_spec.rb
+++ spec/live_order_board_spec.rb
@@ -3,36 +3,33 @@
 require 'LiveOrderBoard'
 
 describe LiveOrderBoard do
-  let(:user_id) { 1 }
+  let(:user_id) { 10 }
   let(:order_quantity) { 3.5 }
   let(:price_per_kg) { 303 }
-  let(:order_type) { 'BUY' }
+  let(:buy) { :BUY }
+  let(:sell) { :SELL }
 
   let(:mock_order_class) { double :order_class, register: mock_order }
   let(:mock_order) { double :order_instane }
 
   let(:live_order_board) { described_class.new(mock_order_class) }
 
-  describe '#register_order' do
+  describe '#buy' do
     it 'delegates to the order object' do
-      expect(mock_order_class).to receive(:register).with(user_id, order_quantity, price_per_kg, order_type)
-      live_order_board.register_order(user_id, order_quantity, price_per_kg, order_type)
+      expect(mock_order_class).to receive(:register).with(user_id, order_quantity, price_per_kg, buy )
+      live_order_board.buy(user_id, order_quantity, price_per_kg)
     end
 
     it 'raises error if invalid user id is submitted' do
-      expect { live_order_board.register_order('user_id', order_quantity, price_per_kg, order_type) }.to raise_error('User id must be a whole number')
+      expect { live_order_board.buy('user_id', order_quantity, price_per_kg) }.to raise_error('User id must be a whole number')
     end
 
     it 'raises error if invalid order quantity is submitted' do
-      expect { live_order_board.register_order(user_id, 'order_quantity', price_per_kg, order_type) }.to raise_error('Order quantity must be a number(kg)')
+      expect { live_order_board.buy(user_id, 'order_quantity', price_per_kg) }.to raise_error('Order quantity must be a number(kg)')
     end
 
     it 'raises error if invalid price is submitted' do
-      expect { live_order_board.register_order(user_id, order_quantity, 'price_per_kg', order_type) }.to raise_error('Price per kg must be a number(£)')
-    end
-
-    it 'raises error if invalid type id is submitted' do
-      expect { live_order_board.register_order(user_id, order_quantity, price_per_kg, 'order_type') }.to raise_error("Order type must be 'BUY' or 'SELL'")
+      expect { live_order_board.buy(user_id, order_quantity, 'price_per_kg') }.to raise_error('Price per kg must be a number(£)')
     end
   end
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -3,14 +3,27 @@
 require 'Order'
 
 describe Order do
-  let(:Order) { subject }
   describe '#register' do
-    it 'instantiates a new order with specified details' do
-      order = Order.register(10, 3.5, 303, :BUY)
+    let(:user_id) { 10 }
+    let(:order_quantity) { 3.5 }
+    let(:price_per_kg) { 303 }
+    let(:buy) { :BUY }
+    let(:sell) { :SELL }
+
+    it "instantiates an order with type 'BUY'" do
+      order = Order.register(user_id, order_quantity, price_per_kg, buy)
       expect(order.user_id).to eq 10
       expect(order.quantity).to eq 3.5
       expect(order.price_per_kg).to eq 303
       expect(order.type).to eq :BUY
+    end
+
+    it "instantiates an order with type 'SELL'" do
+      order = Order.register(user_id, order_quantity, price_per_kg, sell)
+      expect(order.user_id).to eq 10
+      expect(order.quantity).to eq 3.5
+      expect(order.price_per_kg).to eq 303
+      expect(order.type).to eq :SELL
     end
   end
 end
