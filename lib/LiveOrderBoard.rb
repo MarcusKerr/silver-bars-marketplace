92362235368f5078b6193d0be48f5758171b7984
Marcus <marcus.kerr94@gmail.com>
2019-01-19 17:23:25 +0000

    Feature: register order - GREEN

--- lib/LiveOrderBoard.rb
+++ lib/LiveOrderBoard.rb
@@ -1,4 +1,13 @@
 # frozen_string_literal: true
 
+require_relative 'Order'
+
 class LiveOrderBoard
+  def initialize(order = Order)
+    @order = order
+  end
+
+  def register_order(user_id, order_quantity, price_per_kg, order_type)
+    @order.register(user_id, order_quantity, price_per_kg, order_type)
+  end
 end
