6f9c746271505df5e5cdb250b9ee97678c1bdeb0
Marcus <marcus.kerr94@gmail.com>
2019-01-22 12:07:12 +0000

    Refactor: merged order spec, more detailed test

--- spec/merged_order_spec.rb
+++ spec/merged_order_spec.rb
@@ -3,19 +3,16 @@
 require 'MergedOrder'
 
 describe MergedOrder do
-  let(:user_ids) { [10, 11, 12] }
-  let(:order_ids) { [101, 101, 102] }
-  let(:quantity)       { 3.5 }
-  let(:price_per_kg)   { 303 }
-  let(:buy)            { :BUY }
-  let(:sell)           { :SELL }
+  let(:mock_order) { double :order, user_id: 10, quantity: 3.5, price_per_kg: 303, type: :BUY }
+  let(:mock_order_2) { double :order, user_id: 100, quantity: 35, price_per_kg: 303, type: :BUY }
 
-  let(:mock_original_orders)  { [] }
-  let(:mock_grouped_orders)   { [] }
+  let(:mock_original_orders)   { [mock_order, mock_order_2] }
+  let(:mock_grouped_orders)   { [[mock_order, mock_order_2]] }
 
   describe '#create' do
-    it 'returns an array' do
-      expect(MergedOrder.create(mock_grouped_orders, mock_original_orders)).to be_kind_of(Array)
+    it 'returns an array of merged orders' do
+      merged_orders = MergedOrder.create(mock_grouped_orders, mock_original_orders)
+      expect(merged_orders[0]).to be_instance_of described_class
     end
   end
 end
